e04bb98bcd06e58f812c8ede79ee308e27854848
RickeySerna <rickeyserna7@live.com>
2024-09-09 15:28:38 -0500

    Added error messaging for bad date values. If a user uses the two date arg launch method, but one of the args they pass is not in date format at all, an error box will appear letting them know that now.

--- BTTransactionStatsGUI.py
+++ BTTransactionStatsGUI.py
@@ -462,11 +462,22 @@ class MainWindow(QMainWindow):
             self.toggle_calendar_button.setText("Hide Calendar")
             
 def convertToYYYY(date):
-    if date and len(date.split('/')[2]) == 2:
-        parts = date.split('/')
+    # Split the date based on any slashes.
+    parts = date.split('/')
+    
+    # Checking if the date is already in the YYYY format with the length of the third part being 4. If so, we just immediately return the date and end things there.
+    # Also making sure that there are 3 parts to begin with, like a normal date.
+    if len(parts) == 3 and len(parts[2]) == 4:
+        return date
+    # Checking if the date is in the MM/DD/YY format, with the third parts length being 2.
+    elif len(parts) == 3 and len(parts[2]) == 2:
+        # If so, we append "20" to the start of it.
+        # We don't need to worry about 90s (or before) dates here since no such dates would contain BT transactions.
         parts[2] = "20" + parts[2]
         return '/'.join(parts)
-    return date
+    # If the else condition triggers, it means the value passed was not in the date format at all. So we raise a ValueError.
+    else:
+        raise ValueError("Invalid date(s) passed.")
 
 def main():
     app = QApplication(sys.argv)
