1ef2c21b585db5381088fdeda781629b00223f67
RickeySerna <rickeyserna7@live.com>
2024-09-30 15:44:12 -0500

    Added a new entry point to simplify running the project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [2.1.0] - 2024-09-30
+### Changed
+- Added a new entry point to simplify running the project.
+
 ## [2.0.0] - 2024-09-28
 ### Changed
 - Restructured into a full project style architecture.
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Braintree-Transaction-Statistics-GUI',
     version='2.0.0',
-    packages=['BT-stats-GUI'],
+    packages=find_packages(),
     url='',
     license='',
     author='Rickey Serna',
     author_email='rickeyserna7@live.com',
-    description='A Python project which creates a GUI displaying statistics on transactions in a Braintree gateway.'
+    description='A Python project which creates a GUI displaying statistics on transactions in a Braintree gateway.',
+    entry_points={
+        'console_scripts': [
+            'runstats=BT_stats_GUI.main:main',
+        ],
+    },
 )
